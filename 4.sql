d5765d22b659a62b332ed56d85a7ecfcba3eb648
Milrak Pereira Pessoa <milrak.pessoa@gmail.com>
2021-08-01 11:17:01 -0400

    Crie uma coluna para saber o preço unitário do item de venda, crie um script para atualizar os dados já existentes e logo em seguida uma trigger para preencher o campo;

---
+++ 4.sql
@@ -0,0 +1,46 @@
+select * from sale_item si 
+
+-- criando coluna
+alter table sale_item add column unit_price numeric;
+
+
+--script para preencher o valor
+do
+$$
+declare 
+	produto record;
+	cmd_update varchar default '';
+begin
+	for produto in select * from product p where exists (select * from sale_item si where si.id_product=p.id)
+	loop
+	cmd_update := format ('update sale_item set unit_price=%s where id_product=%s ;', produto.sale_price, produto.id);
+	execute cmd_update;
+end loop;
+end
+$$
+
+--trigger para preencher o campo em novas compras.
+create or replace function fn_insert_unit_price_sale_item() returns trigger as 
+$$
+begin 
+	new.unit_price := (select sale_price from product where id=new.id_product);
+return new;
+end
+$$
+language plpgsql;
+
+-- vinculando gatinho
+create trigger tg_insert_unit_price_sale_item
+	before insert on sale_item 
+	for each row
+	execute function fn_insert_unit_price_sale_item();
+
+
+--Test
+INSERT INTO public.sale_item
+(id_sale, id_product, quantity, created_at, modified_at, active)
+VALUES(500001, 1, 1, now(), now(), true);
+
+select * from sale_item si where id=500001;
+
+	
